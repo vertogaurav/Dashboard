c4591cf0d9e7eee711917a7a8c5bcb994e868885
Your Name <NDH00046@Nmcorp.Nissan.Biz>
2020-05-06 23:16:25 +0530

    SideNav Toggle Works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
 
-
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
-    
-    
-    
-   
-     ],
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     DefaultModule,
-  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layouts/default/default.component.ts
+++ src/app/layouts/default/default.component.ts
@@ -2,14 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-default',
-  templateUrl: './default.component.html',
+  templateUrl:'./default.component.html',
   styleUrls: ['./default.component.scss']
 })
 export class DefaultComponent implements OnInit {
-
+  sidebarOpen=false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  sideBarToggler(event){
+    this.sidebarOpen=!this.sidebarOpen;
   }
 
 }
