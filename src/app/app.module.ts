4cedf3f9f6649982eeb89dfc84733c686e515e41
Your Name <NDH00046@Nmcorp.Nissan.Biz>
2020-05-06 12:04:37 +0530

    default component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
+import { DefaultModule } from './layouts/default/default.module';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    
    
      ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
+    DefaultModule,
   
   ],
   providers: [],
