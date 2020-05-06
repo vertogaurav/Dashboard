0763059c4d3d79209f30a0bfa399adfaa84708e0
Your Name <NDH00046@Nmcorp.Nissan.Biz>
2020-05-06 11:38:28 +0530

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+   
+     ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
